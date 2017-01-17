8b5cc581e400a02e05d2a803d01451d5ead46b2d
syzer3@gmail.com <syzer3@gmail.com>
2017-01-17 08:07:43 +0100

    parse Reuters article

---
+++ bin/parseReutersArticle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const { readFile, writeFile } = require('../lib')
+const dataDir = __dirname + '/../data/'
+
+const app =
+    readFile(dataDir + 'example.scraped.article.txt')
+        .map(e => e.split(/\n|\t/gi))
+        .map(e => e.filter(f => !!f))
+        .map(e => e.map(f => f.trim()))
+        // currently rake is unstable
+        .map(e => e.map(f => f.replace(/"/gi,'')))
+        .map(e => e.map(f => f.replace(/'/gi,'’')))
+        .map(e => e.join(' '))
+        .map(e => [e])
+        .map(JSON.stringify)
+        .chain(contents => writeFile(dataDir + 'example.scraped.articles.json', contents))
+
+app.fork(console.error, console.log)
