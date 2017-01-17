69462882c6e119628cde69f004717aec8fb7b8a6
syzer3@gmail.com <syzer3@gmail.com>
2017-01-17 01:15:19 +0100

    parsing liiip article

---
+++ bin/parseLiipArticle.js
@@ -0,0 +1,22 @@
+const R = require('ramda')
+const { map, filter, none, split, isEmpty, join, trim, replace } = R
+const { readFile, writeFile } = require('../lib')
+const dataDir = __dirname + '/../data/'
+
+const parseFile = R.pipe(
+    split(/\n|\t/gi),
+    filter(none(isEmpty)),
+    map(trim),
+    map(replace(/"/gi, '')),
+    map(replace(/'/gi, '’')),
+    join(' '),
+    e => [e],
+    JSON.stringify
+)
+
+const app =
+    readFile(dataDir + 'example.scraped.liip.txt')
+        .map(parseFile)
+        .chain(contents => writeFile(dataDir + 'example.scraped.articles2.json', contents))
+
+app.fork(console.error, console.log)
