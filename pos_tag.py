bd78ab482646347db04822b2e049d5ee7db9e5ba
syzer3@gmail.com <syzer3@gmail.com>
2017-01-26 08:27:21 +0100

    python pos tag

--- pos_tag.py
+++ pos_tag.py
@@ -1,2 +1,11 @@
 # fight with python
 import nltk
+
+# nltk.download()
+
+sentence = 'Thank you @joscha and @kevinmoilar for doing an awesome job on the foodways offer!'
+
+tokens = nltk.word_tokenize(sentence)
+
+print(tokens)
+
