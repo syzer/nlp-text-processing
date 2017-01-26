2a7e605c8873d47b1f5e24d13bea48242bcd912f
syzer3@gmail.com <syzer3@gmail.com>
2017-01-26 19:56:32 +0100

    looking for definitions

--- tfidf.js
+++ tfidf.js
@@ -3,15 +3,13 @@ const n = require('natural')
 const R = require('ramda')
 const lib = require('./lib/natural.async')
 
-const { concat, merge } = require('ramda')
-
 // Term Freq Inverse Document Frequency
 const tfidf = lib.newTfidf()
 
 const phd2009 = require('./data/phd2009.acknowledgements.json')
 const scrapedArticle = require('./data/example.scraped.articles.json')
 
-const docs = R.concat(R.concat(phd2009, scrapedArticle), [
+const docs = phd2009.concat(scrapedArticle, [
     `Each approach has merits. It is usually cheaper (approach 1) to kill a superfluous feature at the requirements stage, before it has wasted implementation resources. This is also better for the morale of the team: developers get frustrated when something they imple- mented gets discarded; that form of waste is worse than tossing out a requirement before anything has been done with it. On the other hand, agilists are right that sometimes the best way (approach 2) to find out if something will be useful is to build it, show it, and see whether it fits.`,
     `Sometimes, but not always. The problem here is dogmatism. Upfront requirements are useful; iterative development is useful. Condemning either of these two complemen- tary techniques in the name of some absolutist ideology does not help projects, but actu- ally harms them.`,
     `The agile criticism is right on target when it lamba sts the huge requirements documents, sometimes running into the thousands of pages, that some bureaucratic environ- ments demand. While describing every single detail in advance is necessary for some life-critical systems (typically embedded systems, for example in transportation), for most business systems such documents are overkill; they become so complex that it is hard to get them right (contradictions and ambiguities creep in), and so unwieldy that they end up forgotten on a shelf rather than being used for the development.`,
@@ -66,7 +64,13 @@ const wordnet = lib.newWordNet()
 // http://sentiwordnet.isti.cnr.it/
 // http://wordnet.princeton.edu
 wordnet.lookupAsync('otter')
-    .then(R.map(R.props(['ptrs', 'gloss', 'synonyms'])))
+    .then(R.map(R.props(['synsetOffset', 'pos', 'ptrs', 'gloss', 'synonyms'])))
     // .then(console.log)
 // 'freshwater carnivorous mammal having webbed and clawed feet and dark brown fur ',
 
+// since we have synsetOffset
+wordnet
+    .getAsync(2447450, 'n')
+    .then(R.prop('gloss'))
+    .then(console.log)
+// freshwater carnivorous mammal having webbed and clawed feet and dark brown fur
