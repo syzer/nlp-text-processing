60ebfebcb2150afd88ff6a3e351b223539f1a40e
syzer3@gmail.com <syzer3@gmail.com>
2017-05-18 12:20:00 +0200

    tests

--- damerauLevenshtein.test.js
+++ damerauLevenshtein.test.js
@@ -1,7 +1,17 @@
+// nodemon -w ./ -x 'cat damerauLevenshtein.test.js | node -p'
+const natural = require('natural')
 const damerau = require('./damerauLevenshtein')
-const distance = require('natural').LevenshteinDistance
+const levenshtein = natural.LevenshteinDistance
+
+const opts = {
+    insertion_cost: 1,
+    deletion_cost: 4,
+    substitution_cost: 4
+}
 
 // (u'jellyfish', u'smellyfish')
-distance('abba', 'baab')
-damerau('abba', 'baab')
+levenshtein('abba', 'baab', opts)
+damerau('abba', 'baab', opts)
 
+levenshtein("ones", "onez", opts)
+damerau("ones", "onez", opts)
